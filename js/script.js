0a81429edbfbc1fe1ef33f6792ca4c7181d124a5
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-07-23 15:40:41 +0200

    completato esercizio

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-const cells = 100;
-console.log('cells', cells, typeof cells);
-
 const gridContainer = document.getElementById('grid-container');
 const playButton = document.getElementById('play-button');
 const diffSelect = document.getElementById('difficulty');
@@ -9,9 +6,6 @@ playButton.addEventListener('click', function () {
     gridContainer.innerHTML = '';
 
     let cells;
-
-    console.log(diffSelect.value);
-
     if (diffSelect.value == '1') {
         cells = 100;
     } else if (diffSelect.value == '2') {
@@ -20,6 +14,11 @@ playButton.addEventListener('click', function () {
         cells = 49;
     }
 
+    const bombs = generateBombs(cells);
+    console.log('Bombs:', bombs);
+
+    let score = 0;
+
     for (let i = 0; i < cells; i++) {
         const newCell = document.createElement('div');
         newCell.innerHTML = i + 1;
@@ -27,20 +26,39 @@ playButton.addEventListener('click', function () {
         if (cells == 100) {
             newCell.classList.add('easy');
         }
-        else if (cells == '81') {
+        else if (cells == 81) {
             newCell.classList.add('medium');
         }
-        else if (cells == '49') {
+        else if (cells == 49) {
             newCell.classList.add('hard');
         }
 
-
         newCell.addEventListener('click', function () {
-            newCell.classList.toggle('color-light-coral');
-
-            console.log(newCell.textContent);
+            if (bombs.includes(parseInt(newCell.textContent))) {
+                newCell.classList.add('color-light-coral');
+                alert(`Hai persooo! Il tuo punteggio è di ${score}.`);
+                gridContainer.innerHTML = '';
+            } else {
+                newCell.classList.add('color-blue');
+                score++;
+                if (score === cells - 16) {
+                    alert(`Hai vintooo! Il tuo punteggio è di ${score}.`);
+                    gridContainer.innerHTML = '';
+                }
+            }
         });
 
         gridContainer.append(newCell);
     }
 });
+
+function generateBombs(maxCells) {
+    const bombArray = [];
+    while (bombArray.length < 16) {
+        const randomNum = Math.floor(Math.random() * maxCells) + 1;
+        if (!bombArray.includes(randomNum)) {
+            bombArray.push(randomNum);
+        }
+    }
+    return bombArray;
+}
