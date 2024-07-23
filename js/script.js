e41a780850350f07716477b42e8edf2f5f9c056c
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-07-23 12:40:49 +0200

    Copiato vecchio esercizio e incollato in questo

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
-// Commento singola riga
+const cells = 100;
+console.log('cells', cells, typeof cells);
 
-/*
+const gridContainer = document.getElementById('grid-container');
+const playButton = document.getElementById('play-button');
 
-    Commento multipla riga
+playButton.addEventListener('click', function () {
+    gridContainer.innerHTML = '';
 
-*/
+    for (let i = 0; i < cells; i++) {
+        const newCell = document.createElement('div');
+        newCell.innerHTML = i + 1;
+
+        newCell.addEventListener('click', function () {
+            newCell.classList.toggle('color-light-coral');
+
+            console.log(newCell.textContent);
+        });
+
+        gridContainer.append(newCell);
+    }
+});
