5360677e7416bb58210816ec8cd659c240e046a4
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-07-23 13:10:29 +0200

    Aggiunto le difficolta

--- js/script.js
+++ js/script.js
@@ -3,9 +3,22 @@ console.log('cells', cells, typeof cells);
 
 const gridContainer = document.getElementById('grid-container');
 const playButton = document.getElementById('play-button');
+const diffSelect = document.getElementById('difficulty');
 
 playButton.addEventListener('click', function () {
     gridContainer.innerHTML = '';
+
+    let cells;
+
+    console.log(diffSelect.value);
+
+    if (diffSelect.value == '1') {
+        cells = 100;
+    } else if (diffSelect.value == '2') {
+        cells = 81;
+    } else if (diffSelect.value == '3') {
+        cells = 49;
+    }
 
     for (let i = 0; i < cells; i++) {
         const newCell = document.createElement('div');
